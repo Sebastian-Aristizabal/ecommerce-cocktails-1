19e4274e65ac77f6cbde8275d6e721760a54f2a0
Sebastian Aristizabal <sebastinn@hotmail.com>
2023-02-11 14:34:00 -0500

    some validations in store and cocktail models, number_currency in every value when it is necessary

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,4 +2,5 @@ class Cocktail < ApplicationRecord
   has_one_attached :photo
   belongs_to :store
   belongs_to :category
+  validates :name, :ml, :ingredients, :description, :price, :discount, :photo, presence: true
 end
