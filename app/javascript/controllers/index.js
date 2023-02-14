a0602762d9eeeacff6a1b1e5cd7ddcb64f1e7352
Sebastian Aristizabal <sebastinn@hotmail.com>
2023-02-13 22:03:49 -0500

    price with stumulus, it is not working

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,8 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
-
-import PriceFormatController from "./priceformat_controller"
-application.register("priceformat", PriceFormatController)
+import VerificadorController from "./verificador_controller"
+application.register("verificador", VerificadorController)
---
+++ app/javascript/controllers/verificador_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+
+  connect() {
+    console.log("no quiere funcionar");
+  }
+
+
+  static targets = [ "input" ]
+
+  formatNumber(event) {
+  // skip for arrow keys
+    if(event.which >= 37 && event.which <= 40){
+        event.preventDefault();
+    }
+
+    let num = this.inputTarget.value.replace(/,/gi, "");
+    let num2 = num.split(/(?=(?:\d{3})+$)/).join(",");
+    console.log(num2);
+  }
+
+}
