2a06cd0722b5fc41c8d9c06ae3d69a3d600d57d8
Sebastian-Aristizabal <sebastinn@hotmail.com>
2023-04-24 12:40:55 -0500

    añadir webpack-boilerplate y comentar las validaciones de time_open y time_close

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,12 +3,12 @@ class Store < ApplicationRecord
   has_many :cocktails, dependent: :destroy
   validates :name, :address, :time_open, :time_close, presence: true
 
-  validate :hour_invalid
-  before_save :hour_invalid
+  # validate :hour_invalid
+  # before_save :hour_invalid
 
-  def hour_invalid
-    if self.time_open <= self.time_close
-      self.errors.add :time_open, "can not be less or equal than time close \n"
-    end
-  end
+  # def hour_invalid
+  #   if self.time_open <= self.time_close
+  #     self.errors.add :time_open, "can not be less or equal than time close \n"
+  #   end
+  # end
 end
